432c7316bb595b8b0544e7f3b077a471e5537902
Marco Luzi <marco@webkinder.ch>
2024-06-14 15:22:03 +0200

    Trying to optimize language switcher

--- app/Livewire/LanguageSwitcher.php
+++ app/Livewire/LanguageSwitcher.php
@@ -5,11 +5,11 @@ namespace App\Livewire;
 use App\Models\Language;
 use App\Models\Option;
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class LanguageSwitcher extends Component
 {
     public $languages;
-
     public $selectedLanguageCode;
     public $selectedLanguageName;
 
@@ -22,21 +22,27 @@ class LanguageSwitcher extends Component
     public function loadSelectedLanguage()
     {
         $option = Option::where('name', 'selected_language')->first();
-        $this->selectedLanguageCode = $option ? $option->value : null;
-        $this->selectedLanguageName = $this->languages->where('code', $this->selectedLanguageCode)->first()->name ?? null;
+        $this->selectedLanguageCode = $option ? $option->value : $this->languages->first()->code;
+        $this->updateSelectedLanguageName();
+    }
+
+    public function updateSelectedLanguageName()
+    {
+        $language = $this->languages->where('code', $this->selectedLanguageCode)->first();
+        $this->selectedLanguageName = $language ? $language->name : __('Select language');
     }
 
     public function chooseLanguage($languageCode)
     {
         $this->selectedLanguageCode = $languageCode;
-        $this->selectedLanguageName = $this->languages->where('code', $this->selectedLanguageCode)->first()->name;
+        $this->updateSelectedLanguageName();
         $this->saveSelectedLanguage();
-
+        $this->dispatch('languageChanged', $languageCode);
     }
 
     public function saveSelectedLanguage()
     {
-        $option = Option::updateOrCreate(
+        Option::updateOrCreate(
             ['name' => 'selected_language'],
             ['value' => $this->selectedLanguageCode]
         );
