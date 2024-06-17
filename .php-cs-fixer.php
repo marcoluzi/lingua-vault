9cc7bb0f9afc69ad7b9da339fa48753d996368ff
Marco Luzi <marco@webkinder.ch>
2024-06-17 07:32:59 +0200

    Setup new tall project with laravel v11

---
+++ .php-cs-fixer.php
@@ -0,0 +1,27 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+$config = new Config();
+
+return $config->setFinder($finder)
+    ->setRules([
+        '@PSR2' => true,
+    ])
+    ->setRiskyAllowed(true)
+    ->setUsingCache(false);
