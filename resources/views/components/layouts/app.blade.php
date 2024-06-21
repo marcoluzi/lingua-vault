759f3d4b9055a82d6db6e35edb506a0141f8ac1e
Marco Luzi <marco@webkinder.ch>
2024-06-21 07:55:02 +0200

    Add container styles to app layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <x-ui.desktop-sidebar />
         <x-ui.mobile-header />
         <main class="py-10 lg:pl-72">
-            <div class="px-4 sm:px-6 lg:px-8">
+            <div class="container mx-auto sm:px-6 lg:px-8">
                 @yield('content')
             </div>
         </main>
