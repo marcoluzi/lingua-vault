ed344c68dc639b7192af75e3089752eb3d227893
Marco Luzi <dev@luzimarco.ch>
2024-10-25 14:11:30 +0200

    Creating base lexeme model and migration

---
+++ database/migrations/2024_10_25_114206_create_lexemes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Support\Enums\Languages;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lexemes', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->string('meaning');
+            $table->string('language');
+            $table->unsignedTinyInteger('e_factor')->default(2.5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lexemes');
+    }
+};
