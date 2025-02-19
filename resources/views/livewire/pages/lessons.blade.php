75d011c70d109e5629720eaf696cb34e392ce442
Marco Luzi <dev@luzimarco.ch>
2025-02-19 14:21:38 +0100

    Applying changes of blade formatter
    Signed-off-by: Marco Luzi <dev@luzimarco.ch>

--- resources/views/livewire/pages/lessons.blade.php
+++ resources/views/livewire/pages/lessons.blade.php
@@ -5,7 +5,10 @@
             <div class="md:max-w-xs w-full">
                 <x-select-menu class="max-w-48" :items="$sortItems" :selectedItem="$selectedSortItem" label="{{ __('Sort by') }}" />
             </div>
-            <a href="{{ route('lessons.create') }}" class="transition rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{ __('Create new lesson') }}</a>
+            <a href="{{ route('lessons.create') }}"
+                class="transition rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                {{ __('Create new lesson') }}
+            </a>
         </div>
         <ul role="list" class="divide-y divide-gray-100 mt-8 md:mt-16">
             @foreach ($lessons as $lesson)
@@ -14,22 +17,26 @@
                         <div class="flex items-start gap-x-3">
                             <p class="text-sm font-semibold leading-6 text-gray-900">{{ $lesson['title'] }}</p>
                             @if ($lesson['progress'] === 0)
-                                <p class="mt-0.5 whitespace-nowrap rounded-md bg-green-50 px-1.5 py-0.5 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/20">
+                                <p
+                                    class="mt-0.5 whitespace-nowrap rounded-md bg-green-50 px-1.5 py-0.5 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/20">
                                     {{ __('Not started') }}
                                 </p>
                             @elseif ($lesson['progress'] === 100)
-                                <p class="mt-0.5 whitespace-nowrap rounded-md bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                                <p
+                                    class="mt-0.5 whitespace-nowrap rounded-md bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                                     {{ __('Complete') }}
                                 </p>
                             @else
-                                <p class="mt-0.5 whitespace-nowrap rounded-md bg-yellow-50 px-1.5 py-0.5 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+                                <p
+                                    class="mt-0.5 whitespace-nowrap rounded-md bg-yellow-50 px-1.5 py-0.5 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
                                     {{ __(':percent% completed', ['percent' => $lesson['progress']]) }}
                                 </p>
                             @endif
                         </div>
                         <div class="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
                             <p class="whitespace-nowrap">{{ __('Last practiced:') }}
-                                <time datetime="{{ \Carbon\Carbon::parse($lesson['updated_at'])->format('Y-m-d') }}">{{ \Carbon\Carbon::parse($lesson['updated_at'])->format('j F, Y') }}</time>
+                                <time
+                                    datetime="{{ \Carbon\Carbon::parse($lesson['updated_at'])->format('Y-m-d') }}">{{ \Carbon\Carbon::parse($lesson['updated_at'])->format('j F, Y') }}</time>
                             </p>
                             <svg viewBox="0 0 2 2" class="h-0.5 w-0.5 fill-current">
                                 <circle cx="1" cy="1" r="1" />
@@ -40,59 +47,41 @@
                         </div>
                     </div>
                     <div class="flex flex-none items-center gap-x-4">
-                        <a href="{{ route('lessons.read', ['lessonId' => $lesson['id']]) }}" class="transition hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block">
+                        <a href="{{ route('lessons.read', ['lessonId' => $lesson['id']]) }}"
+                            class="transition hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block">
                             {{ __('Open lesson') }}
                         </a>
                         <div x-data="{ open: false }" class="relative flex-none">
-                            <button
-                                type="button"
-                                class="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900"
-                                id="lesson-menu-{{ $loop->iteration }}-button"
-                                :aria-expanded="open.toString()"
-                                aria-haspopup="true"
-                                @click="open = !open"
-                            >
+                            <button type="button" class="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900"
+                                id="lesson-menu-{{ $loop->iteration }}-button" :aria-expanded="open.toString()"
+                                aria-haspopup="true" @click="open = !open">
                                 <span class="sr-only">{{ __('Open options') }}</span>
-                                <svg
-                                    class="h-5 w-5"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                    aria-hidden="true"
-                                    data-slot="icon"
-                                >
-                                    <path d="M10 3a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM10 8.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM11.5 15.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
+                                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
+                                    data-slot="icon">
+                                    <path
+                                        d="M10 3a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM10 8.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM11.5 15.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
                                 </svg>
                             </button>
                             <div class="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
-                                role="menu"
-                                aria-orientation="vertical"
-                                aria-labelledby="lesson-menu-{{ $loop->iteration }}-button"
-                                tabindex="-1"
-                                x-show="open"
-                                x-transition:enter="transition ease-out duration-10"
+                                role="menu" aria-orientation="vertical"
+                                aria-labelledby="lesson-menu-{{ $loop->iteration }}-button" tabindex="-1"
+                                x-show="open" x-transition:enter="transition ease-out duration-10"
                                 x-transition:enter-start="transform opacity-0 scale-95"
                                 x-transition:enter-end="transform opacity-100 scale-100"
                                 x-transition:leave="transition ease-in duration-75"
                                 x-transition:leave-start="transform opacity-100 scale-100"
-                                x-transition:leave-end="transform opacity-0 scale-95"
-                                @click.away="open = false"
-                            >
+                                x-transition:leave-end="transform opacity-0 scale-95" @click.away="open = false">
                                 {{-- TODO: Edit --}}
-                                <a
-                                    href="#"
+                                <a href="#"
                                     class="block px-3 py-1 text-sm leading-6 text-gray-900 hover:bg-gray-50"
-                                    role="menuitem"
-                                    tabindex="-1"
-                                >
+                                    role="menuitem" tabindex="-1">
                                     {{ __('Edit') }}
                                     <span class="sr-only">{{ $lesson['title'] }}</span>
                                 </a>
                                 <button
                                     class="block px-3 py-1 text-sm leading-6 text-gray-900 hover:bg-gray-50 w-full text-left"
-                                    role="menuitem"
-                                    tabindex="-1"
-                                    wire:click="$dispatch('openModal', { component: 'components.delete-lesson-modal', arguments: { lessonId: {{ $lesson['id'] }} }})"
-                                >
+                                    role="menuitem" tabindex="-1"
+                                    wire:click="$dispatch('openModal', { component: 'components.delete-lesson-modal', arguments: { lessonId: {{ $lesson['id'] }} }})">
                                     {{ __('Delete') }}
                                 </button>
                             </div>
