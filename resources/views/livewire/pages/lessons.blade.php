660ed28f7d45e4a480a0d80e82b1e605e6a68855
Marco Luzi <dev@luzimarco.ch>
2025-02-20 13:02:53 +0100

    Updating lessons page to utilize blade icons
    Signed-off-by: Marco Luzi <dev@luzimarco.ch>

--- resources/views/livewire/pages/lessons.blade.php
+++ resources/views/livewire/pages/lessons.blade.php
@@ -89,12 +89,7 @@
     @else
         <div class="flex justify-center items-center mt-8 md:mt-16">
             <div class="text-center">
-                <svg class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                    aria-hidden="true">
-                    <path vector-effect="non-scaling-stroke" stroke-linecap="round" stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
-                </svg>
+                <x-icon-solid.folder-xmark class="mx-auto h-12 w-12" />
                 <h3 class="mt-2 text-sm font-semibold text-gray-900">{{ __('No lessons') }}</h3>
                 <p class="mt-1 text-sm text-gray-500">{{ __('Get started by creating a new lesson.') }}</p>
                 <div class="mt-6">
